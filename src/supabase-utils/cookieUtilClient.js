5dd1d53e5d7cd687178d391bddd3794d7e5ea9a1
Isaac J <jimenez.isaac.jr@gmail.com>
2025-02-08 20:21:25 -0800

    Add cookie handling for next.js

---
+++ src/supabase-utils/cookieUtilClient.js
@@ -0,0 +1,23 @@
+import {createServerClient} from "@supabase/ssr";
+import { cookies } from "next/headers";
+export const getSupabaseCookiesUtilClient = () => {
+    const cookieStore = cookies();
+    return createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        {
+            cookies: {
+                getAll() {
+                    return cookieStore.getall()
+                },
+                setAll(cookiesToSet) {
+                    try{
+                        cookiesToSet.forEach(({ name, value, options }) => {cookieStore.set(name, value, options)})
+                    } catch (error) {
+                        console.log("Failed to set cookies: ",error)
+                    }
+                }
+            }
+        }
+    )
+}
---
+++ src/supabase-utils/reqRes.js
@@ -0,0 +1,32 @@
+// A Supabase client with server-side cookie handling for Next.js middleware
+// Ensures cookies are properly set and retrieved during server-side requests
+import {createServerClient} from "@supabase/ssr";
+import {NextResponse} from "next/server";
+export const getSupabaseReqResClient = ({ request }) => {
+    let response = {
+        value: NextResponse.next({request})
+    }
+    const supabase = createServerClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        {
+            cookies: {
+                getAll() {
+                    return request.cookies.getAll()
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value, options }) => {
+                        request.cookies.set(name, value)
+                    })
+                    response.value = NextResponse.next({
+                        request,
+                    })
+                    cookiesToSet.forEach(({ name, value, options }) => {
+                        response.value.cookies.set(name, value, options)
+                    })
+                },
+            },
+        }
+    )
+    return { supabase, response } //
+}
