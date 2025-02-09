b9f41ff3ebd44c6c2ddd93d457f2d231d7708759
Isaac J <jimenez.isaac.jr@gmail.com>
2025-02-08 21:14:52 -0800

    add login page w/ links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import {Login} from "@/app/Login";
-export default function Home() {
+import { Login } from "./Login";
+
+export default function LoginPage({ searchParams }) {
+  const wantsMagicLink = searchParams.magicLink === "yes"
   return (
-      <Login/>
+      <Login isPasswordLogin={!wantsMagicLink} />
   )
 }
