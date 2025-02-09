6ad31ce42c632644ff39a6ba65493b7896561b9d
Isaac J <jimenez.isaac.jr@gmail.com>
2025-02-08 20:47:21 -0800

    add login page w/ css

---
+++ src/app/Login.tsx
@@ -0,0 +1,10 @@
+export const Login = () => {
+    return (
+        <form>
+            <article style={{maxWidth:"420 px", margin:"auto"}}>
+                <header>Login</header>
+                <strong>Hello Login!</strong>
+            </article>
+        </form>
+    )
+}
