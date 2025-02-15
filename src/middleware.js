7195d901d41296eabdc596de6cf62d89e173ffbc
Isaac J <jimenez.isaac.jr@gmail.com>
2025-02-14 19:33:19 -0800

    add route matcher

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,9 @@
 import { getSupabaseReqResClient } from "@/supabase-utils/reqRes"
 export async function middleware(request) {
     const { supabase, response } = getSupabaseReqResClient({ request })
+
     return response.value
 }
+export const config = {
+    matcher: ["/((?!.*\\.).*)"],
+};
