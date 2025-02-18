8cf2cd4f57b22fb670f29369c133243e9d859796
Isaac J <jimenez.isaac.jr@gmail.com>
2025-02-17 17:10:21 -0800

    working middle ware

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ export async function middleware(request) {
     const { supabase, response } = getSupabaseReqResClient({ request });
     await supabase.auth.getSession();
 
-        return response.value
+    return response.value
 }
 export const config = {
     matcher: ["/((?!.*\\.).*)"],
